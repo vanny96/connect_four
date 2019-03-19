4f7d215530e496b35980825e1c46465b6bc8e925
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-19 12:13:39 +0100

    Create test for #put_piece function

---
+++ connect_four.rb
@@ -0,0 +1,4 @@
+class ConnectFour
+  def put_piece column
+  end
+end
---
+++ spec/grid_spec.rb
@@ -0,0 +1,29 @@
+require './connect_four.rb'
+
+describe ConnectFour do
+  describe "#put_piece" do
+    it "puts the piece on the first row" do
+      game = ConnectFour.new
+      expect(game.put_piece 2).to eql([0,1])  
+    end
+    xit "stacks pieces on over the other" do
+      game = ConnectFour.new
+      game.put_piece 1
+      expect(game.put_piece 1).to eql([1,0])  
+    end
+    xit "doesn't stack if on different columns" do
+      game = ConnectFour.new
+      game.put_piece 1
+      expect(game.put_piece 2).to eql([0,1])
+    end
+    xit "doesn't let you put pieces outside the grid" do
+    game = ConnectFour.new
+    expect(game.put_piece 8).not_to eql([0,7])  
+    end
+    xit "doesn't let you put piece over the top" do
+      game = ConnectFour.new
+      6.times{game.put_piece 5}
+      expect(game.put_piece 5).not_to eql([6,4])
+    end
+  end
+end
