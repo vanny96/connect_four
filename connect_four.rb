782bfdd73999b9aa57b5805405149b711bb71660
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-19 12:21:42 +0100

    Created grid for the game

--- connect_four.rb
+++ connect_four.rb
@@ -1,4 +1,25 @@
 class ConnectFour
+  attr_accessor :grid
+
+  def initialize
+    @grid =  create_grid
+    
+  end
   def put_piece column
   end
+  private
+  def create_grid
+    grid = []
+    6.times do
+      row = []
+      7.times do
+        row << " "
+      end
+      grid << row
+    end
+    grid
+  end
 end
+
+cacca = ConnectFour.new
+print cacca.grid
